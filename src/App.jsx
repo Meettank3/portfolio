051cb5a80d42d341848ee3619124b6d66c8233fa
Meettank3 <125813530+Meettank3@users.noreply.github.com>
2025-09-28 19:12:24 +0530

    Done FeaturesCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import LogoSection from "./components/LogoSection";
 import NavBar from "./components/NavBar";
+import FeaturesCard from "./sections/FeaturesCard";
 import { Hero } from "./sections/Hero";
+import LogoSection from "./sections/LogoSection";
 import ShowcaseSection from "./sections/ShowcaseSection";
 
 const App = () =>{
@@ -10,6 +11,7 @@ const App = () =>{
         <Hero />
         <ShowcaseSection />
         <LogoSection />
+        <FeaturesCard />
         </>
     )
 }
