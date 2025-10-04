2ec35d55c523a2b081a49226e12a89b08fab3bbb
Meettank3 <125813530+Meettank3@users.noreply.github.com>
2025-10-05 02:42:51 +0530

    Portfolio Complete email line changed

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -22,11 +22,13 @@ const Contact = () => {
     setLoading(true); // Show loading state
 
     try {
+      emailjs.init(import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY);
       await emailjs.sendForm(
+        
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         formRef.current,
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        //import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       );
 
       // Reset form and stop loading
